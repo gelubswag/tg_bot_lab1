aa300dd668299433e191660b1e2432eb0d623a7d
gelubswag <doshdik35@gmail.com>
2024-10-12 00:23:03 +0300

    Add files via upload

---
+++ bottelebot.py
@@ -0,0 +1,109 @@
+
+import telebot
+from cfg import *
+import MukeshAPI
+import gtts
+import glob
+
+""""IMAGE START"""
+def gen_img(msg):
+        
+    try:
+        img = MukeshAPI.api.ai_image(msg.text)
+        bot.send_photo(msg.chat.id, img, msg.text)
+        return
+    except:
+        bot.send_message(msg.chat.id,"Не удалось отправить изображение")
+        bot.register_next_step_handler(msg, gen_img)
+        
+def send_img(msg):
+    
+    try:
+        bot.send_photo(msg.chat.id, msg.text)
+        return
+    except Exception as ex:
+        bot.send_message(msg.chat.id,f"Не удалось отправить изображение: {ex}")
+        bot.register_next_step_handler(msg, send_img)
+        
+@bot.callback_query_handler(func=lambda call: call.data == IMG_BTNS[1][1])
+def link(call:telebot.types.CallbackQuery):
+    msg = call.message
+    bot.edit_message_text("Отправьте ссылку", msg.chat.id,msg.id)
+    bot.register_next_step_handler(msg, send_img)
+
+@bot.callback_query_handler(func=lambda call: call.data == IMG_BTNS[0][1])
+def link(call:telebot.types.CallbackQuery):
+    msg = call.message
+    bot.edit_message_text("Отправьте запрос", msg.chat.id,msg.id)
+    bot.register_next_step_handler(msg, gen_img)
+# image
+@bot.callback_query_handler(func=lambda call: call.data == START_BTNS[0][1])
+def image_btn(call:telebot.types.CallbackQuery):
+    msg = call.message
+    keyboard = telebot.types.InlineKeyboardMarkup()
+    buttons = [telebot.types.InlineKeyboardButton(text=text, callback_data=callback_data) for text, callback_data in IMG_BTNS]
+    keyboard.add(*buttons)
+    bot.edit_message_text("Работа с изображением", msg.chat.id,msg.id,reply_markup=keyboard)
+"""IMAGE END"""
+
+
+"""AUDIO START"""
+def send_audio(msg:telebot.types.Message):
+    try:
+        bot.send_audio(chat_id=msg.chat.id,audio=msg.text)
+    except Exception as ex:
+        bot.send_message(msg.chat.id,f"Не удалось отправить аудио: {ex}")
+        bot.register_next_step_handler(msg, send_audio)
+
+@bot.callback_query_handler(func=lambda call: call.data == AUDIO_BTNS[1][1])
+def audios(call:telebot.types.CallbackQuery):
+    msg = call.message
+
+    keyboard = telebot.types.InlineKeyboardMarkup()
+    keyboard.add(*[telebot.types.InlineKeyboardButton(i,callback_data=i) for i in AUDIO_SONGS.keys()])
+    bot.edit_message_text("Выберите аудио", msg.chat.id,msg.id,reply_markup=keyboard)
+
+@bot.callback_query_handler(func=lambda call: call.data in AUDIO_SONGS.keys())
+def send_audio(call):
+    msg = call.message
+    bot.delete_message(msg.chat.id,msg.id)
+    with open(f"{AUDIO_SONGS[call.data]}", "rb") as f: bot.send_audio(msg.chat.id,f.read())
+
+def gen_audio(msg:telebot.types.Message):
+    try:
+        tts = gtts.gTTS(text=msg.text, lang='en')
+        tts.save("req.mp3")
+        with open("req.mp3","rb") as f: bot.send_audio(chat_id=msg.chat.id,audio=f)
+    except Exception as ex:
+        bot.send_message(msg.chat.id,f"Не удалось отправить аудио: {ex}")
+        bot.register_next_step_handler(msg, gen_audio)
+
+@bot.callback_query_handler(func=lambda call: call.data == AUDIO_BTNS[0][1])
+def tts_audio(call:telebot.types.CallbackQuery):
+    msg = call.message
+    bot.edit_message_text("Отправьте text-to-speech", msg.chat.id,msg.id)
+    bot.register_next_step_handler(msg, gen_audio)
+
+@bot.callback_query_handler(func=lambda call: call.data == START_BTNS[1][1])
+def audio_btn(call:telebot.types.CallbackQuery):
+    msg = call.message
+    keyboard = telebot.types.InlineKeyboardMarkup()
+    buttons = [telebot.types.InlineKeyboardButton(text=text, callback_data=callback_data) for text, callback_data in AUDIO_BTNS]
+    keyboard.add(*buttons)
+    bot.edit_message_text("Работа с изображением", msg.chat.id,msg.id,reply_markup=keyboard)
+    
+"""AUDIO END"""
+@bot.message_handler(commands=["start"])
+def start(msg:telebot.types.Message):
+    chat_id = msg.chat.id
+    keyboard = telebot.types.InlineKeyboardMarkup()
+    buttons = [telebot.types.InlineKeyboardButton(text=text, callback_data=callback_data) for text, callback_data in START_BTNS]
+    keyboard.add(*buttons)
+    
+    return bot.send_message(chat_id,
+                            START_MSG,
+                            reply_markup= keyboard)
+
+# @bot.message_handler(content_types=START_BTNS)
+# def
+bot.polling()
---
+++ cfg.py
@@ -0,0 +1,26 @@
+import telebot
+import numpy
+import glob
+TOKEN = "7697069280:AAHgetIdqq8SbFJVUUYvYmyjHiT8aPkk1Co"
+bot = telebot.TeleBot(TOKEN)
+
+AUDIO_DIR = "audio"
+AUDIO_SONGS = {i.replace(f"{AUDIO_DIR}\\","").replace("_"," ").replace(".mp3","") : i for i in glob.glob(f"{AUDIO_DIR}/*.mp3")}
+START_MSG = "Лабораторная работа №1"
+
+START_BTNS = [
+    ("Работа с изображениями (выслать сгенерированное или готовое по запросу)", "image_button"),# image button
+    ("Работа с аудиофайлами (выслать сгенерированный или готовый по запросу)", "audio_button"), # audio button
+    ("Получить ссылку на публичный репозиторий с исходниками бота", "source_button") # source button
+]
+
+IMG_BTNS = [
+    ("Сгенерировать сообщение по запросу", "generate_img"),
+    ("Отправить изображение по ссылке", "link_img"),
+            ]
+
+AUDIO_BTNS = [
+    ("Сгенерировать text-to-speech", "generate_audio"),
+    ("Отправить аудио по ссылке", "link_sound")
+]
+
